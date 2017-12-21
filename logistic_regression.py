339a636ba692b78e54b251377e8fd608bd92fda5
Branislav Holländer <branislav.hollander@ge.com>
2017-12-21 14:30:54 +0100

    Updated logistic regression example with SGD

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,16 +1,21 @@
-"""Performs logistic regression using gradient descent."""
+"""Performs logistic regression using stochastic gradient descent."""
 
 import numpy as np
 
-NUM_EPOCHS = 1000000
+NUM_CLASSES = 2
+NUM_FEATURES = 3
 
-LEARNING_RATE = 0.01
-LOSS_DISPLAY_FREQUENCY = 10000
+NUM_EPOCHS = 100000
+NUM_EXAMPLES = 10
+
+LEARNING_RATE = 0.001
+LOSS_DISPLAY_FREQUENCY = 1000
 
 def logistic(x): # logistic function
     return 1.0 / (1.0 + np.exp(-x))
 
 def gradxentropy(y_hat, y):
+    y_hat = np.minimum(np.maximum(y_hat, 1e-5), 1.0 - 1e-5) # avoid division by zero
     return ((1-y)/(1-y_hat))-(y/y_hat)
 
 def gradlogistic(x): # gradient of the logistic function
@@ -25,45 +30,52 @@ def gradb(z, y_hat, y): # bias gradient
     return grads.mean(axis=0)
 
 def loss(y_hat, y): # cross entropy loss (cost)
-    y_hat = np.minimum(np.maximum(y_hat, 1e-5), 1.0 - 1e-5) # avoid division by zero
+    y_hat = np.minimum(np.maximum(y_hat, 1e-5), 1.0 - 1e-5) # avoid logarithm of zero
     l = -y * np.log(y_hat) - (1-y) * np.log(1-y_hat)
     return l.sum()
 
 def accuracy(y_hat, y):
-     correct_prediction = np.equal(np.argmax(y_hat, axis=1), np.argmax(y, axis=1))
+     correct_prediction = np.equal(np.argmax(y_hat, axis=1), np.argmax(y, axis=1)).astype(int)
+     correct_prediction = correct_prediction.astype(float)
      return correct_prediction.mean()
 
 def forward(x, w, b): # forward pass through network
     return x.dot(w) + b
 
 def main():
-    x = np.zeros((10, 2)) # 2-dimensional input; first dimension = batch size
-    x[:,0] = np.random.rand(10)
-    x[:,1] = [100.0, 50.0, 60.0, 30.0, 80.0, 6.0, 70.0, 20.0, 90.0, 70.0]
-    x[:,1] = x[:,1] / 100.0
+    x = np.zeros((NUM_EXAMPLES, NUM_FEATURES)) # 2-dimensional input; first dimension = batch size
+    x[:,0] = [-2.0, 6.0, -3.0, 2.0, -9.0, 2.0, -1.0, 10.0, -7.0, 7.0]
+    x[:,1] = [10.0, -5.0, 6.0, -3.0, 8.0, -6.0, 7.0, -2.0, 9.0, -7.0]
+    x[:,2] = [0.0, 0.0, 0.0, 1.0, -9.0, 6.0, -5.0, 2.0, -9.0, 1.0]
+    x = x / 10.0
 
-    y = np.zeros((10, 2))
+    y = np.zeros((NUM_EXAMPLES, NUM_CLASSES))
     y[:,0] = [1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0] # 2-class output (one-hot encoding)
     y[:,1] = [0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0] # 2-class output (one-hot encoding)
 
-    w = np.random.rand(2, 2) # randomly initialize weights
-    b = np.zeros(2) # initialize biases to zero
+    w = np.random.rand(NUM_FEATURES, NUM_CLASSES) # randomly initialize weights
+    b = np.zeros(NUM_CLASSES) # initialize biases to zero
 
-    # perform gradient descent
-    for i in range(1, NUM_EPOCHS):
-        z = forward(x, w, b)
-        y_hat = logistic(z) # prediction
+    # perform stochastic gradient descent (no batch, just 1 example per iteration)
+    for i in range(0, NUM_EPOCHS):
+        for j in range(0, NUM_EXAMPLES):
+            x_ex = np.reshape(x[j,:], (1,NUM_FEATURES))
+            y_ex = np.reshape(y[j,:], (1,NUM_CLASSES))
 
-        # calculate gradients
-        gw = gradw(x, z, y_hat, y)
-        gb = gradb(z, y_hat, y)
-        
-        # update weights
-        w = w - LEARNING_RATE * gw
-        b = b - LEARNING_RATE * gb
+            z = forward(x_ex, w, b)
+            y_hat = logistic(z) # prediction
+
+            # calculate gradients
+            gw = gradw(x_ex, z, y_hat, y_ex)
+            gb = gradb(z, y_hat, y_ex)
+            
+            # update weights
+            w = w - LEARNING_RATE * gw
+            b = b - LEARNING_RATE * gb
 
         # calculate loss in regular intervals
         if i % LOSS_DISPLAY_FREQUENCY == 0:
+            y_hat = logistic(forward(x, w, b))
             l = loss(y_hat, y)
             a = accuracy(y_hat, y)
             print("Loss in {0}th epoch: {1:5f}, Accuracy: {2}".format(i, l, a))
@@ -71,8 +83,8 @@ def main():
     print("Final weights: w = {0}, b = {1}".format(w, b))
 
     # predict some numbers
-    x_pred = np.array([[1,6],[2,7],[3,8],[4,9],[5,10]])
-    x_pred = x_pred / 33.0
+    x_pred = np.array([[1,6,5],[2,7,3],[3,8,-2],[-4,-9,0],[5,-10,1]])
+    x_pred = x_pred / 10.0
     y_pred = np.argmax(forward(x_pred, w, b), axis=1)
     print("Predictions: {0}".format(y_pred))
 
